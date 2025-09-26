99cd2cb58a87ffeb61579f5a5a3f75e07c1df7af
[_MateO_] <justmatthias300@gmail.com>
2025-09-26 11:46:17 +0200

    update

--- Zadanie galeria obrazów/skrypt.js
+++ Zadanie galeria obrazów/skrypt.js
@@ -5,3 +5,47 @@ function zmiana(src, numer){
     obecny.src = src;
     numer_zdj = numer;
 }   
+function poprzednie(){
+    const aktualne = document.getElementById("obecny");
+    numer_zdj--;
+    if(numer_zdj==0){
+        numer_zdj=5;
+    }
+    if(numer_zdj==1){
+        aktualne.src = "1.jpg";
+    }
+    if(numer_zdj==2){
+        aktualne.src = "2.jpg";
+    }
+    if(numer_zdj==3){
+        aktualne.src = "3.jpg";
+    }
+    if(numer_zdj==4){
+        aktualne.src = "4.jpg";
+    }
+    if(numer_zdj==5){
+        aktualne.src = "5.jpg";
+    }
+}
+function nastepny(){
+    const aktualne =document.getElementById("obecny");
+    numer_zdj++;
+    if(numer_zdj==6){
+        numer_zdj=1;
+    }
+    if(numer_zdj==1){
+        aktualne.src = "1.jpg";
+    }
+    if(numer_zdj==2){
+        aktualne.src = "2.jpg";
+    }
+    if(numer_zdj==3){
+        aktualne.src = "3.jpg";
+    }
+    if(numer_zdj==4){
+        aktualne.src = "4.jpg";
+    }
+    if(numer_zdj==5){
+        aktualne.src = "5.jpg";
+    }
+}
