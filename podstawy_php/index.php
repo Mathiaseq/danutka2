73aaec8b3d9de374e01383575442747d12794f01
[_MateO_] <justmatthias300@gmail.com>
2025-09-30 13:56:19 +0200

    PHP

--- podstawy_php/index.php
+++ podstawy_php/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PL-pl">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Podstawy Języka PHP</title>
 </head>
 <body>
     <h1>Podstawy PHP</h1>
+    <p>Rozpoczynanie i zakończenie poleceń PHP odbywa się za pomocą znaczników: </p>
+    <ul>
+        <li>Znak otwierajacy - znak mniejszości&lt;?php</li>
+        <li>Znak zamykający - ?znak wiekszości - ?></li>
+    </ul>
 </body>
 </html>
 
 
 <?php
-
+echo "<h2>Mój pierwszy skrypt PHP</h2>";
 ?>
