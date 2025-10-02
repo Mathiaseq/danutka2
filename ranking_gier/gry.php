154c7d556a47ec3d5fd72d2ecac69b7a6c8b01c3
[_MateO_] <justmatthias300@gmail.com>
2025-10-02 08:32:57 +0200

    Update gry.php

--- ranking_gier/gry.php
+++ ranking_gier/gry.php
@@ -13,15 +13,25 @@
     <h3>Top 5 gier w tym miesiącu</h3>
     <ul>
         skypt1
-        <li></li>
     </ul>
+        <h3>Nasz sklep</h3>
+        <a href="http://sklep.gry.pl">Tu kupisz gry</a>
+        <h3>Stronę wykonał: 
+            <p>389173189378891</p>
+        </h3>
     </section>
     <section id="srodkowy">
-
+    skrypt2
     </section>
     <section id="prawy">
+    <h3>Dodaj nową grę</h3>
+    <form action="dodaj.php" method="POST">
 
+    </form>
     </section>  
+    <footer>
+
+    </footer>
 </body>
 </html>
 <?php
