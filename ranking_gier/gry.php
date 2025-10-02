1ec2a2aaef506fe187c5b975984a409843becef7
[_MateO_] <justmatthias300@gmail.com>
2025-10-02 08:29:09 +0200

    gry php

---
+++ ranking_gier/gry.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Gry komputerowe</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <header>
+    <h1>Ranking gier komputerowych</h1>
+    </header>
+    <section id="lewy">
+    <h3>Top 5 gier w tym miesiącu</h3>
+    <ul>
+        skypt1
+        <li></li>
+    </ul>
+    </section>
+    <section id="srodkowy">
+
+    </section>
+    <section id="prawy">
+
+    </section>  
+</body>
+</html>
+<?php
+
+?>
