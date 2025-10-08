78da734982e8470014c3c884c7aedbcb02785105
[_MateO_] <justmatthias300@gmail.com>
2025-10-08 07:49:45 +0200

    Update index.php

--- biblioteka_szkolna/index.php
+++ biblioteka_szkolna/index.php
@@ -2,9 +2,35 @@
 <html lang="PL-pl">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>BIBLIOTEKA SZKOLNA</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="skrypt.js"></script>
 </head>
 <body>
-    
+    <header>
+        <h2>STRONA BIBLIOTEKI SZKOLNEJ WIEDZAMIN</h2>
+    </header>
+    <section>
+        <h3>Nasze dzisiejsze propozycje</h3>
+        <table border="1">
+            <tr><th>Autor</th><th>Tytuł</th><th>Katalog</th></tr>
+            <?php
+                
+            ?>
+        </table>
+    </section>
+    <main>
+        <article id="lewy">
+            <img src="ksiazka1.jpg" alt="okładka książki">
+        </article>
+        <article id="srodkowy">
+            <img src="ksiazka2.jpg" alt="okładka książki">
+        </article> 
+        <article id="prawy">
+            <img src="ksiazka3.jpg" alt="okładka książki">
+        </article>
+        <p></p>
+    </main>
+    <footer>Stronę wykonał: 138971984719</footer>
 </body>
 </html>
