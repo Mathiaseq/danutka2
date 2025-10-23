b0c494baca4a34462cd45333135e453a1933d0a9
[_MateO_] <justmatthias300@gmail.com>
2025-10-23 10:26:29 +0200

    Update skrypt.js

--- smoki/skrypt.js
+++ smoki/skrypt.js
@@ -1,3 +1,17 @@
-function baza(){
-    const blokbaza=document.getElementById("blokbaza");
+function baza(aktualnyBlok){
+    const blokBaza=document.getElementById("blokBaza");
+    const pierwszy=document.getElementById("pierwszy");
+    const blokOpisy=document.getElementById("blokOpisy");
+    const drugi=document.getElementById("drugi");
+    const blokGaleria=document.getElementById("blokGaleria");
+    const trzeci=document.getElementById("trzeci");
+    if(aktualnyBlok == pierwszy){
+        blockBaza.style.backgroundColor="MistyRose";
+        blokOpisy.style.backgroundColor="#FFAEA5";
+        blockGaleria.style.backgroundColor="#FFAEA5";
+        pierwszy.style.dispaly="block";
+    }
+}
+function opisy(aktualnyBlok){
+    
 }
