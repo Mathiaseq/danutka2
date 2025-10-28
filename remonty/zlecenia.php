c9fc495761fa84246dcc2cd0c21d842a1cd503f2
[_MateO_] <justmatthias300@gmail.com>
2025-10-28 13:39:51 +0100

    Update zlecenia.php

--- remonty/zlecenia.php
+++ remonty/zlecenia.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Remonty</title>
-    <link rel="stylesheet" type="text/css" href="styl2.css">
+    <link rel="stylesheet" type="text/css" href="styl.css">
 </head>
 <body>
     <header>
@@ -15,24 +15,52 @@
             <a href="https://remonty.pl" target="_blank">Partnerzy</a>
         </nav>
         <aside>
-            <img src="tapeta_prawa.png" alt="uslugi">
-            <img src="tapeta_lewa.png" alt="uslugi">
+            <img src="tapeta_lewa.png" alt="usługi"><br>
+            <img src="tapeta_prawa.png" alt="usługi"><br>
+            <img src="tapeta_lewa.png" alt="usługi">
         </aside>
-        <section id="lewy">
+        <section id="lewa">
             <h2>Dla klientów</h2>
-            <form action="skrypt1.php" method="post">
-                <label for="ilosc_pracownikow">Ilu co najmniej pracowników potrzebujesz?</label>
-                <input type="number" id="ilosc_pracownikow" name="ilosc_pracownikow">
-                <input type="submit" value="Szukaj firm">
-            </form>
-            skrypt1
+                <form action="zlecenia.php" method="post">
+                    Ilu co najmniej pracowników potrzebujesz<br>
+                    <input type="number" name="liczPracownik">
+                    <button name="submit">Szukaj firm</button>
+                </form>
+        <?php
+            $link=mysqli_connect('localhost','root','','remonty');
+            $liczPracownik=$_POST['liczPracownik']??NULL;
+            if(isset($liczPracownik)){
+                $zap2="SELECT `nazwa_firmy`, `liczba_pracownikow` FROM `wykonawcy` WHERE liczba_pracownikow>=40;";
+                $wynik2=mysqli_query($link,$zap2);
+                while($wiersz=mysqli_fetch_array($wynik2)){
+                    echo"<p>$wiersz[0], $wiersz[1]</p>";
+                }
+            }
+        ?>
         </section>
-        <section id="srodkowy">
+        <section id="srodkowa">
             <h2>Dla wykonawców</h2>
-            <form action="zlecenia.php" method="post">
-                <input type="radio" value="checked" name="wybor">malowanie<br>
-                <input type="radio" name="wybor">gipsowanie
-            </form>
-    </main>
+                <form action="zlecenia.php" method="post">
+                   <select>
+                        <?php
+                            $zap3="SELECT miasto FROM klienci ORDER BY miasto ASC;";
+                            $wynik3=mysqli_query($link,$zap3);
+                            while($opcja=mysqli_fetch_array($wynik3)){
+                                echo"<option value='$opcja[0]'>$opcja[0]</option>";
+                            }
+                        ?>
+                   </select><br>
+                   <input type="radio" value="checked" name="wybor">Malowanie<br>
+                   <input type="radio" name="wybor">Gipsowanie<br>
+                   <button name="submit">Szukaj klientów</button>
+                </form>
+                <ul>
+                    skrypt3
+                </ul>
+            </section>
+        </main>
+    <footer>
+        <p>Stronę wykonał: <strong>Twoja nazwa</strong></p>
+    </footer>
 </body>
 </html>
