ab2875a2e29c063014f3bd36f42e6a150ddcd1bd
[_MateO_] <justmatthias300@gmail.com>
2025-11-04 14:09:31 +0100

    Update medica.php

--- Medica/medica.php
+++ Medica/medica.php
@@ -3,10 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Przychodnia Medica</title>
-    <link rel="icon" href="favicon.ico" type="obraz2.png">
+    <link rel="icon" href="obraz2.png" type="image/png">
     <link rel="stylesheet" type="text/css" href="styl.css">
 </head>
 <body>
-    
-</body>
+    <header>
+        </header>
+        <article>
+
+        </article>
+        <main>
+            <section id="pierwsza">
+
+            </section>
+            <section id="druga">
+
+            </section>
+            <section id="trzecia">
+
+            </section>
+            
+        </main>
+</body> 
 </html>
