6965ec1169552980232084a34f1dbf202c8c2fd9
[_MateO_] <justmatthias300@gmail.com>
2025-11-04 14:06:26 +0100

    medica

---
+++ Medica/medica.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Przychodnia Medica</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
