ecb74a906077876620fbd116a4732091f51e6352
[_MateO_] <justmatthias300@gmail.com>
2025-11-07 11:29:36 +0100

    Update index.php

--- buty/index.php
+++ buty/index.php
@@ -2,9 +2,22 @@
 <html lang="PL-pl">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Document</title
+    <link rel="Stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    
+    <header>
+        <h1>Obuwie męskie</h1>
+    </header>
+    <main>
+        <form action="zamow.php" method="POST">
+            <label>Model:<select>
+                <option>40</option>
+            </select></label>
+        </form>
+    </main>
+    <footer>
+        <p>Autor strony: 41897412978</p>
+    </footer>
 </body>
 </html>
