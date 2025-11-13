2efad1dc45cbcc38aa355d20e25874066bade3ea
[_MateO_] <justmatthias300@gmail.com>
2025-11-13 12:45:50 +0100

    final

--- wyszukiwarka_miast/index.php
+++ wyszukiwarka_miast/index.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PL-pl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wyszukiwarka miast</title>
+    <link rel="shortcut icon" href="fav.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    
+    <header>
+        <img src="baner.jpg" alt="Polska">
+    </header>
+    <aside id="lewy_gorny">
+        <h4>Podaj początek nazwy miasta</h4>
+        <form action="index.php" method="post">
+            <input type="text" name="miasto">
+            <input type="submit" value="Szukaj">
+</aside>
+<aside id="prawy">
+    <h1>Wyniki wyszukiwania miast z uwzględnieniem filtra:</h1>
+</aside>
+<aside id="lewy_dolny">
+    <p>Egzamin INF.03</p>
+    <p>Autor: 391037810382</p>
+</aside>
+
 </body>
 </html>
