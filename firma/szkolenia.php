a44d22650939df91fbfc68a59ff1b2dcfd3056f0
[_MateO_] <justmatthias300@gmail.com>
2025-11-13 08:42:36 +0100

    Update szkolenia.php

--- firma/szkolenia.php
+++ firma/szkolenia.php
@@ -6,12 +6,12 @@ $conn = mysqli_connect("localhost", "root", "", "firma");
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
         <header>
             <img src="baner.jpg" alt="Szkolenia">
         </header>
-
         <nav>
             <ul>
                 <li><a href="index.html">Strona główna</a></li>
@@ -21,13 +21,13 @@ $conn = mysqli_connect("localhost", "root", "", "firma");
 
         <main>
             <?php
-                
+
             ?>
         </main>
 
         <footer>
             <h2>Firma szkoleniowa, ul. Główna 1, 23-456 Warszawa</h2>
-            <p>Autor: 000000000000000000000000</p>
+            <p>Autor: 31703418903718</p>
         </footer>
     </body>
 </html>
