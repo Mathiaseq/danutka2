8ff743bf0e4ce48bed2733a9deda39af687f666a
[_MateO_] <justmatthias300@gmail.com>
2025-11-13 08:24:24 +0100

    html + css + php

---
+++ firma/szkolenia.php
@@ -0,0 +1,19 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "firma");
+?>
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+
+
+<?php
+$mysqli_close($conn);
+?>
