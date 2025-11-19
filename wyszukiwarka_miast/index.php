75793e30871f4c3bfa2b6b768e4d4ba57ddc9142
[_MateO_] <justmatthias300@gmail.com>
2025-11-19 07:24:36 +0100

    wyszukiwarka miast finale

--- wyszukiwarka_miast/index.php
+++ wyszukiwarka_miast/index.php
@@ -1,28 +1,59 @@
 <!DOCTYPE html>
-<html lang="PL-pl">
-<head>
-    <meta charset="UTF-8">
-    <title>Wyszukiwarka miast</title>
-    <link rel="shortcut icon" href="fav.png" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-    <header>
-        <img src="baner.jpg" alt="Polska">
-    </header>
-    <aside id="lewy_gorny">
-        <h4>Podaj początek nazwy miasta</h4>
-        <form action="index.php" method="post">
-            <input type="text" name="miasto">
-            <input type="submit" value="Szukaj">
-</aside>
-<aside id="prawy">
-    <h1>Wyniki wyszukiwania miast z uwzględnieniem filtra:</h1>
-</aside>
-<aside id="lewy_dolny">
-    <p>Egzamin INF.03</p>
-    <p>Autor: 391037810382</p>
-</aside>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Wyszukiwarka miast</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="shortcut icon" href="fav.png" type="image/x-icon">
+    </head>
+    <body>
+        <main>
+            <header>
+                <img src="baner.jpg" alt="Polska">
+            </header>
 
-</body>
+            <div id="lewy1">
+                <h4>Podaj początek nazwy miasta</h4>
+                <form action="index.php" method="post">
+                    <input type="text" name="miasto" id="miasto">
+                    <input type="submit" value="Szukaj" id="szukaj" name="szukaj">
+                </form>
+            </div>
+
+            <div id="prawy">
+                <h1>Wyniki wyszukiwania miast z uwzględnieniem filtra:</h1>
+                <?php
+                    if (isset($_POST['szukaj'])) {
+                        $miasto = $_POST['miasto'];
+                        echo "<p>$miasto</p>";
+
+                        $conn = mysqli_connect("localhost", "root", "", "wykaz");
+                        $sql = "SELECT miasta.nazwa AS miasta_nazwa, wojewodztwa.nazwa AS wojewodztwa_nazwa FROM miasta JOIN wojewodztwa ON wojewodztwa.id = id_wojewodztwa WHERE miasta.nazwa LIKE '$miasto%' ORDER BY miasta.nazwa;";
+                        $result = mysqli_query($conn, $sql);
+
+                        echo "<table>";
+                            echo "<tr>";
+                                echo "<th>Miasto</th>";
+                                echo "<th>Województwo</th>";
+                            echo "</tr>";
+                        while ($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                                echo "<td>".$row['miasta_nazwa']."</td>";
+                                echo "<td>".$row['wojewodztwa_nazwa']."</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+
+                        mysqli_close($conn);
+                    }
+
+                ?>
+            </div>
+
+            <div id="lewy2">
+                <p>Egzamin INF.03</p>
+                <p>Autor: 00000000000000</p>
+            </div>
+        </main>
+    </body>
 </html>
