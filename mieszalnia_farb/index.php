502af75b92689ece70342fc03fd93405ba564f17
[_MateO_] <justmatthias300@gmail.com>
2025-11-19 07:58:17 +0100

    Update index.php

--- mieszalnia_farb/index.php
+++ mieszalnia_farb/index.php
@@ -30,40 +30,56 @@
                     <th>Data odbioru</th>
                 </tr>
                 <?php
-                   $conn = mysqli_connect("localhost", "root", "", "mieszalnia");
+                //    $conn = mysqli_connect("localhost", "root", "", "mieszalnia");
 
-                    if(isset($_POST['wyszukaj'])) {
-                        $dataod = $_POST['dataod']??null;
-                        $datado = $_POST['datado']??null;
-                        $sql = "SELECT nazwisko, imie, zamowienia.id, kod_koloru, pojemnosc, data_odbioru FROM klienci JOIN zamowienia ON klienci.id = id_klienta WHERE data_odbioru >= '$dataod' AND data_odbioru <= '$datado' ORDER BY data_odbioru;";
-                        $result = mysqli_query($conn, $sql);
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<tr>";
-                            echo "<td>" . $row["id"] . "</td>";
-                            echo "<td>" . $row["nazwisko"] . "</td>";
-                            echo "<td>" . $row["imie"] . "</td>";
-                            echo "<td style='background-color: #".$row["kod_koloru"].";'>" . $row["kod_koloru"] . "</td>";
-                            echo "<td>" . $row["pojemnosc"] . "</td>";
-                            echo "<td>" . $row["data_odbioru"] . "</td>";
-                            echo "</tr>";
-                        }
+                //     if(isset($_POST['wyszukaj'])) {
+                //         $dataod = $_POST['dataod']??null;
+                //         $datado = $_POST['datado']??null;
+                //         $sql = "SELECT nazwisko, imie, zamowienia.id, kod_koloru, pojemnosc, data_odbioru FROM klienci JOIN zamowienia ON klienci.id = id_klienta WHERE data_odbioru >= '$dataod' AND data_odbioru <= '$datado' ORDER BY data_odbioru;";
+                //         $result = mysqli_query($conn, $sql);
+                //         while ($row = mysqli_fetch_assoc($result)) {
+                //             echo "<tr>";
+                //             echo "<td>" . $row["id"] . "</td>";
+                //             echo "<td>" . $row["nazwisko"] . "</td>";
+                //             echo "<td>" . $row["imie"] . "</td>";
+                //             echo "<td style='background-color: #".$row["kod_koloru"].";'>" . $row["kod_koloru"] . "</td>";
+                //             echo "<td>" . $row["pojemnosc"] . "</td>";
+                //             echo "<td>" . $row["data_odbioru"] . "</td>";
+                //             echo "</tr>";
+                //         }
+                //     }
+                //     else {
+                //         $sql = "SELECT nazwisko, imie, zamowienia.id, kod_koloru, pojemnosc, data_odbioru FROM klienci JOIN zamowienia ON klienci.id = id_klienta ORDER BY data_odbioru;";
+                //         $result = mysqli_query($conn, $sql);
+                //         while ($row = mysqli_fetch_assoc($result)) {
+                //             echo "<tr>";
+                //             echo "<td>" . $row["id"] . "</td>";
+                //             echo "<td>" . $row["nazwisko"] . "</td>";
+                //             echo "<td>" . $row["imie"] . "</td>";
+                //             echo "<td style='background-color: #".$row["kod_koloru"].";'>" . $row["kod_koloru"] . "</td>";
+                //             echo "<td>" . $row["pojemnosc"] . "</td>";
+                //             echo "<td>" . $row["data_odbioru"] . "</td>";
+                //             echo "</tr>";
+                //         }
+                //     }
+
+                
+                    $link=mysqli_connect("localhost", "root", "", "mieszalnia");
+                    $dataod = $_POST['dataod']??null;
+                    $datado = $_POST['datado']??null;
+                    if(isset($_POST['submit']));
+                    if($dataod!=NULL && $datado!=NULL){
+                        $zap3="SELECT k.nazwisko, k.imie, z.id, z.kod_koloru, z.pojemnosc, z.data_odbioru FROM klienci k, zamowienia z WHERE z.id_klienta = k.Id ORDER BY z.data_dbioru ASC;";
+                        $wynik=mysqli_query($link, $zap3);
                     }
-                    else {
-                        $sql = "SELECT nazwisko, imie, zamowienia.id, kod_koloru, pojemnosc, data_odbioru FROM klienci JOIN zamowienia ON klienci.id = id_klienta ORDER BY data_odbioru;";
-                        $result = mysqli_query($conn, $sql);
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<tr>";
-                            echo "<td>" . $row["id"] . "</td>";
-                            echo "<td>" . $row["nazwisko"] . "</td>";
-                            echo "<td>" . $row["imie"] . "</td>";
-                            echo "<td style='background-color: #".$row["kod_koloru"].";'>" . $row["kod_koloru"] . "</td>";
-                            echo "<td>" . $row["pojemnosc"] . "</td>";
-                            echo "<td>" . $row["data_odbioru"] . "</td>";
-                            echo "</tr>";
-                        }
+                    while(($wiersz=mysqli_fetch_array($wynik))!= NULL){
+                        echo "<tr><td>$wiersz[2]</td><td>$wiersz[0]</td><td>$wiersz[1]</td><td style='background-color:#".$wiersz[3]."'>$wiersz[3]</td>
+                        <td>$wiersz[4]</td><td>$wiersz[5]</td></tr>";
                     }
 
-                    mysqli_close($conn);
+                    
+
+                    mysqli_close($link);
                     ?>
             </table>
         </main>
