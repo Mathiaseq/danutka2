f713300185fc5c5fcaa5c4a02d9adbc588704f78
[_MateO_] <justmatthias300@gmail.com>
2025-12-03 07:50:28 +0100

    galeria final

--- obrazy/galeria.php
+++ obrazy/galeria.php
@@ -1,33 +1,27 @@
-<?php
-    $conn = mysqli_connect("localhost", "root", "", "galeria");
-?>
-
 <!DOCTYPE html>
-<html lang="pl">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Galeria</title>
-        <link rel="stylesheet" href="styl.css">
-    </head>
-    <body>
-        <header>
-            <h1>Zdjęcia</h1>
-        </header>
-
-        <div id="lewy">
-            <h2>Tematy zdjęć</h2>
-            <ol>
-                <li>Zwierzęta</li>
-                <li>Krajobrazy</li>
-                <li>Miasta</li>
-                <li>Przyroda</li>
-                <li>Samochody</li>
-            </ol>
-        </div>
-        
-        <main>
-            <?php
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Galeria</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Zdjęcia</h1>
+    </header>
+    <section id="lewy">
+        <h2>Tematy zdjęć</h2>
+        <ol>
+            <li>Zwierzęta</li>
+            <li>Krajobrazy</li>
+            <li>Miasta</li>
+            <li>Przyroda</li>
+            <li>Samochody</li>
+        </ol>
+    </section>
+    <main>
+         <?php
+         $conn = mysqli_connect("localhost", "root", "", "galeria");
                 $sql = "SELECT plik, tytul, polubienia, imie, nazwisko FROM zdjecia JOIN autorzy ON autorzy_id = autorzy.id ORDER BY nazwisko;";
                 $result = mysqli_query($conn, $sql);
                 while($row = $result -> fetch_array()) {
@@ -47,26 +41,21 @@
                     echo "</div>";
                 }
             ?>
-        </main>
-
-        <div id="prawy">
-            <h2>Najbardziej lubiane</h2>
+    </main>
+    <section id="prawy">
+        <h2>Najbardziej lubiane</h2>
             <?php
                 $sql = "SELECT tytul, plik FROM zdjecia WHERE polubienia >= 100;";
                 $result = mysqli_query($conn, $sql);
                 while($row = $result -> fetch_array()) {
                     echo "<img src='$row[1]' alt='$row[0]'>";
                 }
+                mysqli_close($conn);
             ?>
-            <strong>Zobacz wszystkie nasze zdjęcia</strong>
-        </div>
-
-        <footer>
+        <strong>Zobacz wszystkie nasze zdjęcia</strong>
+    </section>
+     <footer>
             <h5>Stronę wykonał: 34678326784236784236784</h5>
         </footer>
-    </body>
+</body>
 </html>
-
-<?php
-    mysqli_close($conn);
-?>
