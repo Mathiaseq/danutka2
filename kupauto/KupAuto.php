f5eadd5c2b03eb09411a7dd02708857969d9bf8d
[_MateO_] <justmatthias300@gmail.com>
2025-12-04 08:54:59 +0100

    Update KupAuto.php

--- kupauto/KupAuto.php
+++ kupauto/KupAuto.php
@@ -10,7 +10,23 @@
         <h1><em>KupAuto!</em>Internetowy Komis Samochodowy</h1>
     </header>
     <main id="pierwszy">
-        <!--skrypt 1-->
+        <?php
+            $link = mysqli_connect("localhost","root","","kupauto");
+            $zap2="SELECT model, rodznik, przebieg, paliwo, cena, zdjecie FROM samochody WHERE od=10;";
+            $wynik2=mysqli_query($link,$zap2);
+            if($wynik2){
+    while($row = mysqli_fetch_array($wynik2)){
+        echo "<div class='offer'>";
+        echo "<img src='".$row['zdjecie']."' alt='oferta dnia'>";
+        echo "<h4>Oferta Dnia: ".ucfirst($row['model'])."</h4>";
+        echo "<p>Rocznik: ".$row['rodznik'].", przebieg: ".$row['przebieg'].", rodzaj paliwa: ".$row['paliwo']."</p>";
+        echo "<h4>Cena: ".$row['cena']."</h4>";
+        echo "</div>";
+    }
+}else{
+    echo "Błąd zapytania do bazy danych.";
+}
+?>
     </main>
     <main id="drugi">
         <h2>Oferty Wyróżnione</h2>
