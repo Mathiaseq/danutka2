494b61e31ed100d614b03279df5053f6e714ba94
[_MateO_] <justmatthias300@gmail.com>
2025-12-04 08:47:17 +0100

    e

---
+++ kupauto/KupAuto.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Komis aut</title>
+    <link rel="stylesheet" type="text/css" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1><em>KupAuto!</em>Internetowy Komis Samochodowy</h1>
+    </header>
+    <main id="pierwszy">
+        <!--skrypt 1-->
+    </main>
+    <main id="drugi">
+        <h2>Oferty Wyróżnione</h2>
+        <!--skrypt 2-->
+    </main>
+    <main id="trzeci">
+        <h2>Wybierz markę</h2>
+        <form action="KupAuto.php" method="POST">
+            <select name="marka" id="marka">
+                <!--skrypt 3-->
+            </select>
+            <input type="submit" value="szukaj">
+<!--Albo button type="submit"wyszukaj jak nie zadziała or sm shit-->
+        </form>
+        </main>
+    <footer>
+        <p>Stronę wykonał: 739018329180</p>
+        <p><a href="https://firmy.pl/komis"></a>Znajdź nas także</p>
+    </footer>
+</body>
+</html>
