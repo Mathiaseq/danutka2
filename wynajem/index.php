21564e420cb3534946d3586f572c5ba7e3618a91
[_MateO_] <justmatthias300@gmail.com>
2025-12-11 07:58:25 +0100

    Update index.php

--- wynajem/index.php
+++ wynajem/index.php
@@ -27,7 +27,7 @@
         </div>
 
         <footer>
-            <p>Stronę opracował: <a href="https://ee-informatyk.pl/" target="_blank" style="color: #fff;text-decoration: none;font-size: 100%;padding: 0;">EE-Informatyk.pl</a></p>
+            <p>Stronę opracował: 12893763312</p>
         </footer>
     </body>
 </html>
