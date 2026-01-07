8202dfbf92a64f07d0769056d54b70c589a37ecc
[_MateO_] <justmatthias300@gmail.com>
2026-01-07 07:49:50 +0100

    Update uzytkownicy.php

--- nasze_osiedle/uzytkownicy.php
+++ nasze_osiedle/uzytkownicy.php
@@ -18,6 +18,7 @@
 
         <div id="baner2">
             <?php
+                // Skrypt #1
                 $sql = "SELECT COUNT(*) FROM dane;";
                 $result = $conn->query($sql);
 
@@ -41,6 +42,7 @@
         <div id="prawy">
             <h3>Wizytówka</h3>
             <?php
+                // Skrypt #2
                 if(isset($_POST["login"]) && isset($_POST["haslo"])) {
                     echo "<div class='wizytowka'>";
                     if(!empty($_POST["login"]) && !empty($_POST["haslo"])) {
@@ -51,43 +53,40 @@
                         $sql = "SELECT haslo FROM uzytkownicy WHERE login = '$login';";
                         $result = $conn->query($sql);
 
-                        if($result->num_rows == 1) {
-                            while($row = $result -> fetch_array()) {
-                                $haslobaza = $row[0];
-                            }
+                        if ($result->num_rows == 1) {
+                            switch ($result->num_rows) {
+                            case 1:
+                while ($row = $result->fetch_array()) {
+                $haslobaza = $row[0];
+            }
 
-                            if($hash == $haslobaza) {
-                                $sql = "SELECT u.login, d.rok_urodz, d.przyjaciol, d.hobby, d.zdjecie FROM uzytkownicy u INNER JOIN dane d ON u.id = d.id WHERE u.login = '$login';";
-                                $result = $conn->query($sql);
+            if ($hash == $haslobaza) {
+                $sql = "SELECT u.login, d.rok_urodz, d.przyjaciol, d.hobby, d.zdjecie FROM uzytkownicy u INNER JOIN dane d ON u.id = d.id WHERE u.login = '$login';";
+                $result = $conn->query($sql);
 
-                                while($row = $result -> fetch_array()) {
-                                    $wiek = date("Y") - $row[1];
-                                    echo "<img src='$row[4]' alt='osoba'>";
-                                    echo "<h4>$row[0] ($wiek)</h4>";
-                                    echo "<p>hobby: $row[3]</p>";
-                                    echo "<h1><img src='icon-on.png' alt='obraz serca'> $row[2]</h1>";
-                                    echo "<a href='dane.html'>Więcej informacji</a>";
-                                }
-                            }
-
-                            else {
-                                echo "hasło nieprawidłowe";
-                            }
-                        }
-                        else {
-                            echo "login nie istnieje";
-                        }
-                    }
-                    else {
-                        echo "Uzupełnij wszystkie pola";
-                    }
-                    echo "</div>";
+                while ($row = $result->fetch_array()) {
+                    $wiek = date("Y") - $row[1];
+                    echo "<img src='$row[4]' alt='osoba'>";
+                    echo "<h4>$row[0] ($wiek)</h4>";
+                    echo "<p>hobby: $row[3]</p>";
+                    echo "<h1><img src='icon-on.png' alt='obraz serca'> $row[2]</h1>";
+                    echo "<a href='dane.html'>Więcej informacji</a>";
                 }
+            } else {
+                echo "hasło nieprawidłowe";
+            }
+            break;
+        default:
+            echo "login nie istnieje";
+    }
+}
+                    }
+}
             ?>
         </div>
 
         <footer>
-            Stronę wykonał: 3917903278413 </a>
+            Stronę wykonał: 8322163183621
         </footer>
     </body>
 </html>
