67202c6aab59dc52505748a1224d7efb13da86f0
[_MateO_] <justmatthias300@gmail.com>
2026-01-09 11:30:22 +0100

    hurtownia

---
+++ hurtownia/opinie.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Opinie klientów</title>
+    <link rel="stylesheet" type="text/css" href="styl3.css">
+</head>
+<body>
+    <header>
+        <h1>Hurtownia spożywcza</h1>
+    </header>
+    <main>
+        <h2>Opinie naszych klientów</h2>
+        skrypt1
+    </main>
+    <footer id="pierwszy">
+        
+    </footer>
+    <footer id="drugi">
+
+    </footer>
+    <footer id="trzeci">
+
+    </footer>
+    <footer id="czwarty">
+
+    </footer>
+    </footer>
+</body>
+</html>
