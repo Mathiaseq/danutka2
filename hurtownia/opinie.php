f6b3419f7bbd1a24e35f6ece660ce3fdfa0a8d84
[_MateO_] <justmatthias300@gmail.com>
2026-01-09 11:33:56 +0100

    Update opinie.php

--- hurtownia/opinie.php
+++ hurtownia/opinie.php
@@ -14,17 +14,21 @@
         skrypt1
     </main>
     <footer id="pierwszy">
-        
+        <h3>Współpracują z nami</h3>
+        <a href="http://sklep.pl/">Sklep 1</a>
     </footer>
     <footer id="drugi">
-
+        <h3>Nasi top klienci</h3>
+        <ul>
+            <li>skrypt2</li>
+        </ul>
     </footer>
     <footer id="trzeci">
-
+        <h3>Skontaktuj się</h3>
+        <p>telefon: 111222333</p>
     </footer>
     <footer id="czwarty">
-
-    </footer>
+        <h3>Autor: 1980371930</h3>
     </footer>
 </body>
 </html>
