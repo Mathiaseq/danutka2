3784339b7dbe317bdd12a925e6e77c77dc781924
[_MateO_] <justmatthias300@gmail.com>
2026-01-09 11:42:49 +0100

    Update opinie.php

--- hurtownia/opinie.php
+++ hurtownia/opinie.php
@@ -1,34 +1,51 @@
+<?php
+    $conn = mysqli_connect("localhost","root","","hurtownia");
+?>
+
 <!DOCTYPE html>
-<html lang="PL-pl">
-<head>
-    <meta charset="UTF-8">
-    <title>Opinie klientów</title>
-    <link rel="stylesheet" type="text/css" href="styl3.css">
-</head>
-<body>
-    <header>
-        <h1>Hurtownia spożywcza</h1>
-    </header>
-    <main>
-        <h2>Opinie naszych klientów</h2>
-        skrypt1
-    </main>
-    <footer id="pierwszy">
-        <h3>Współpracują z nami</h3>
-        <a href="http://sklep.pl/">Sklep 1</a>
-    </footer>
-    <footer id="drugi">
-        <h3>Nasi top klienci</h3>
-        <ul>
-            <li>skrypt2</li>
-        </ul>
-    </footer>
-    <footer id="trzeci">
-        <h3>Skontaktuj się</h3>
-        <p>telefon: 111222333</p>
-    </footer>
-    <footer id="czwarty">
-        <h3>Autor: 1980371930</h3>
-    </footer>
-</body>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Opinie klientów</title>
+        <link rel="stylesheet" href="styl3.css">
+    </head>
+    <body>
+        <header>
+            <h1>Hurtownia spożywcza</h1>
+        </header>
+
+        <main>
+            <h2>Opinie naszych klientów</h2>
+            <?php
+               
+            ?>
+        </main>
+
+        <div id="stopka1">
+            <h3>Współpracują z nami</h3>
+            <a href="http://skle.pl/">Sklep 1</a>
+        </div>
+
+        <div id="stopka2">
+            <h3>Nasi top klienci</h3>
+            <ol>
+                <?php
+                   
+                ?>
+            </ol>
+        </div>
+
+        <div id="stopka3">
+            <h3>Skontaktuj się</h3>
+            <p>telefon: 111222333</p>
+        </div>
+
+        <div id="stopka4">
+            <h3>Autor: <a href="https://ee-informatyk.pl/" target="_blank" style="color: unset;text-decoration: none;">EE-Informatyk.pl</a></h3>
+        </div>
+    </body>
 </html>
+
+<?php
+    mysqli_close($conn);
+?>
