4efa97a0ab30f46b820509e72bbd4f2b38233758
[_MateO_] <justmatthias300@gmail.com>
2026-01-14 07:46:13 +0100

    Update opinie.php

--- hurtownia/opinie.php
+++ hurtownia/opinie.php
@@ -3,9 +3,11 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="PL-pl">
+<html lang="pl">
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Opinie klientów</title>
         <link rel="stylesheet" href="styl3.css">
     </head>
@@ -17,32 +19,46 @@
         <main>
             <h2>Opinie naszych klientów</h2>
             <?php
-               
+                $sql = "SELECT klienci.zdjecie, klienci.imie, opinie.opinia FROM klienci, opinie, typy WHERE klienci.id = opinie.klienci_id AND typy.id = klienci.typy_id AND typy.id IN (2,3);";
+                $result = $conn->query($sql);
+                
+                while($row = $result -> fetch_array()) {
+                    echo "<div class='opinia'>";
+                        echo "<img src='$row[0]' alt='klient'>";
+                        echo "<div class='cytat'>$row[2]</div>";
+                        echo "<h4>$row[1]</h4>";
+                    echo "</div>";
+                }
             ?>
         </main>
 
-        <footer id="stopka1">
+        <div id="stopka1">
             <h3>Współpracują z nami</h3>
-            <a href="http://sklep.pl/">Sklep 1</a>
-        </footer>
+            <a href="http://skle.pl/">Sklep 1</a>
+        </div>
 
-        <footer id="stopka2">
+        <div id="stopka2">
             <h3>Nasi top klienci</h3>
             <ol>
                 <?php
-                   
+                    $sql = "SELECT imie, nazwisko, punkty FROM klienci ORDER BY punkty DESC LIMIT 3;";
+                    $result = $conn->query($sql);
+
+                    while($row = $result -> fetch_array()) {
+                        echo "<li>$row[0] $row[1], $row[2]</li>";
+                    }
                 ?>
             </ol>
-        </footer>
+        </div>
 
-        <footer id="stopka3">
+        <div id="stopka3">
             <h3>Skontaktuj się</h3>
             <p>telefon: 111222333</p>
-        </footer>
+        </div>
 
-        <footer id="stopka4">
-            <p>Autor: 1738917893</p>
-        </footer>
+        <div id="stopka4">
+            <h3>Autor: 389173891089</h3>
+        </div>
     </body>
 </html>
 
