07b961273bce6ad09f35623e1b2fc0390325d7e8
[_MateO_] <justmatthias300@gmail.com>
2026-01-27 13:24:44 +0100

    biuro

---
+++ wesoła wyprawa/buiro.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="PL-pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Podróże dalekie i bliskie</title>
+    <link rel="stylesheet" type="text/css" href="styl6.css">
+</head>
+<body>
+    <header>
+        <h1>WESOŁA WYPRAWA</h1>
+    </header>
+    <section id ="ciasteczka">
+        skrypt1
+    </section>
+    <aside id="lewy">
+        <table>
+            <tr><td><img src="polska.jpg" alt="zwidzanie Krakowa"></td><td><img src="wlochy.jpg" alt="Wenecja i nie tylko"></td></tr>
+            <tr><td><img src="grecja.jpg" alt="gorące Greckie wyspy"></td><td><img src="hiszpania.jpg" alt="Słoneczna Barcelona"></td></tr>
+        </table>
+    </aside>
+    <aside id="prawy">
+        <h3>Proponujemy wycieczki</h3>
+        <ul>
+            <li>autokarowe
+            <ol>
+            <li>po Polsce z przewodnikiem</li>
+            <li>do Niemiec i Czech</li>
+            </ol></li>
+            <li>samolotem<ol>
+                <li>Wczasy w Grecji</li>
+                <li>Zwiedzanie Barcelony</li>
+                <li>zwiedzanie Wenecji</li>
+            </ol></li>
+        </ul>
+    </aside>
+    <footer>
+
+    </footer>
+
+</body>
+</html>
